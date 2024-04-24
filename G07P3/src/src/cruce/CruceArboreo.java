a9c4c1a5a6edb00a34aa2a89f84a27360c2aa3e4
Jorbis21 <javierorbis@gmail.com>
2024-04-24 14:42:22 +0200

    no entiendo una verga

--- G07P3/src/src/cruce/CruceArboreo.java
+++ G07P3/src/src/cruce/CruceArboreo.java
@@ -12,25 +12,27 @@ public class CruceArboreo implements ICruce {
   public ArrayList<Cromosoma> cruzar(ArrayList<Cromosoma> padres, Random rand, double probCruce) {
     ArrayList<Cromosoma> nuevaGeneracion = new ArrayList<Cromosoma>();
 
-    Cromosoma padre1 = padres.get(0);
-    Cromosoma padre2 = padres.get(1);
+    for(int i = 0; i < padres.size() - padres.size()%2; i+=2) {
+    	Cromosoma padre1 = padres.get(i);
+        Cromosoma padre2 = padres.get(i+1);
 
-    if (rand.nextDouble() < probCruce) {
-      Cromosoma hijo1 = new Cromosoma(padre1);
-      Cromosoma hijo2 = new Cromosoma(padre2);
+        if (rand.nextDouble() < probCruce) {
+          Cromosoma hijo1 = new Cromosoma(padre1);
+          Cromosoma hijo2 = new Cromosoma(padre2);
 
-      Nodo nodoHijo1 = hijo1.getNodoRand();
-      Nodo nodoHijo2 = hijo2.getNodoRand();
+          Nodo nodoHijo1 = hijo1.getNodoRand();
+          Nodo nodoHijo2 = hijo2.getNodoRand();
 
-      hijo1.swapSubtrees(nodoHijo1, nodoHijo2);
-      
-      nuevaGeneracion.add(hijo1);
-      nuevaGeneracion.add(hijo2);
-    } else {
-      nuevaGeneracion.add(new Cromosoma(padre1));
-      nuevaGeneracion.add(new Cromosoma(padre2));
+          hijo1.swapSubtrees(nodoHijo1, nodoHijo2);
+          
+          nuevaGeneracion.add(new Cromosoma(hijo1));
+          nuevaGeneracion.add(new Cromosoma(hijo2));
+        }
+        else {
+        	 nuevaGeneracion.add(new Cromosoma(padre1));
+             nuevaGeneracion.add(new Cromosoma(padre2));
+        }
     }
-
     return nuevaGeneracion;
   }
 
